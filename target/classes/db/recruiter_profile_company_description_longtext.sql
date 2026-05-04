dbc37180ad813587db208e8cdfcbc57783501137
Kaustav1409 <kg1409.pvt@gmail.com>
2026-05-04 06:14:01 +0530

    Removed unnecessary files

--- target/classes/db/recruiter_profile_company_description_longtext.sql
+++
@@ -1,5 +0,0 @@
--- Run against your HireNest database (e.g. hirenest_db) if Hibernate ddl-auto did not widen columns.
--- Verify: SHOW FULL COLUMNS FROM recruiter_profile;
-
-ALTER TABLE recruiter_profile MODIFY company_name VARCHAR(512);
-ALTER TABLE recruiter_profile MODIFY company_description LONGTEXT;
